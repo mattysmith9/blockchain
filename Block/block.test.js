c38202c60e9bbb8158d74991629312b4fe478973
Matty Smith <matthewcsmith18@gmail.com>
2019-06-18 16:10:43 -0400

    genesis tdd

--- Block/block.test.js
+++ Block/block.test.js
@@ -1,3 +1,7 @@
+const Block = require('./block');
+const { GENESIS_DATA } = require('../config');
+const cryptoHash = require('../Crypto-Hash/cryptoHash');
+
 describe('Block', () => {
   const timestamp = 'a-date';
   const lastHash = 'foo-hash';
@@ -8,5 +12,52 @@ describe('Block', () => {
     lastHash,
     hash,
     data
+  });
+
+  it('has a timestamp, lastHash, hash, and data property', () => {
+    expect(block.timestamp).toEqual(timestamp);
+    expect(block.lastHash).toEqual(lastHash);
+    expect(block.hash).toEqual(hash);
+    expect(block.data).toEqual(data);
+  });
+
+  describe('genesis()', () => {
+    const genesisBlock = Block.genesis();
+
+    it('returns a Block instance', () => {
+      expect(genesisBlock instanceof Block).toBe(true);
+    });
+
+    it('returns the genesis data', () => {
+      expect(genesisBlock).toEqual(GENESIS_DATA);
+    });
+  });
+
+  describe('mineBlock()', () => {
+    const lastBlock = Block.genesis();
+    const data = 'mined data';
+    const minedBlock = Block.mineBlock({ lastBlock, data });
+
+    it('returns a Block instance', () => {
+      expect(minedBlock instanceof Block).toBe(true);
+    });
+
+    it('sets the `lastHash` to be the `hash` of the lastBlock', () => {
+      expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+    });
+
+    it('sets the `data`', () => {
+      expect(minedBlock.data).toEqual(data);
+    });
+
+    it('sets a `timestamp`', () => {
+      expect(minedBlock).not.toEqual(undefined);
+    });
+
+    it('creates a SHA-256 `hash` based on the proper inputs', () => {
+      expect(minedBlock.hash).toEqual(
+        cryptoHash(minedBlock.timestamp, lastBlock.hash, data)
+      );
+    });
   });
 });
---
+++ Blockchain/blockchain.js
@@ -0,0 +1,38 @@
+const Block = require('../Block/block');
+const cryptoHash = require('../Crypto-Hash/cryptoHash');
+
+class Blockchain {
+  constructor() {
+    this.chain = [Block.genesis()];
+  }
+
+  addBlock({ data }) {
+    const newBlock = Block.mineBlock({
+      lastBlock: this.chain[this.chain.length - 1],
+      data
+    });
+
+    this.chain.push(newBlock);
+  }
+
+  static isValidChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false;
+    }
+
+    for (let i = 1; i < chain.length; i++) {
+      const { timestamp, lastHash, hash, data } = chain[i];
+      const actualLastHash = chain[i - 1].hash;
+
+      if (lastHash !== actualLastHash) return false;
+
+      const validatedHash = cryptoHash(timestamp, lastHash, data);
+
+      if (hash !== validatedHash) return false;
+    }
+
+    return true;
+  }
+}
+
+module.exports = Blockchain;
