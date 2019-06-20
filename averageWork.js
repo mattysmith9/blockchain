9984833ffe12bf6804227d652e35b5d57ea49b1c
Matty Smith <matthewcsmith18@gmail.com>
2019-06-20 17:54:39 -0400

    hex

---
+++ averageWork.js
@@ -0,0 +1,28 @@
+const Blockchain = require('./Blockchain/blockchain');
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock({ data: 'initial' });
+
+let preTimestamp, nextTimestamp, nextBlock, timeDifference, averageTime;
+
+const times = [];
+
+for (let i = 0; i < 1000; i++) {
+  preTimestamp = blockchain.chain[blockchain.chain.length - 1].timestamp;
+
+  blockchain.addBlock({ data: `block ${i}` });
+  nextBlock = blockchain.chain[blockchain.chain.length - 1];
+
+  nextTimestamp = nextBlock.timestamp;
+  timeDifference = nextTimestamp - preTimestamp;
+  times.push(timeDifference);
+
+  averageTime = times.reduce((total, num) => total + num) / times.length;
+
+  console.log(
+    `Time to mine block: ${timeDifference}ms. Difficulty: ${
+      nextBlock.difficulty
+    }. Average time: ${averageTime}ms`
+  );
+}
