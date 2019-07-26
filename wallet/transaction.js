97503eb20d8749ba3807b9b9a5691f35ed6abb93
Matty Smith <matthewcsmith18@gmail.com>
2019-07-26 15:10:51 -0400

    transaction

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -29,6 +29,34 @@ class Transaction {
   }
 
   static validTransaction(transaction) {
+    const {
+      input: { address, amount, signature },
+      outputMap
+    } = transaction;
+
+    const outputTotal = Object.values(outputMap).reduce(
+      (total, outputAmount) => total + outputAmount
+    );
+
+    if (amount !== outputTotal) {
+      console.error(`Invalid transaction from ${address}`);
+
+      return false;
+    }
+
+    if (
+      !verifySignature({
+        publicKey: address,
+        amount,
+        data: outputMap,
+        signature
+      })
+    ) {
+      console.error(`Invalid signature from ${address}`);
+
+      return false;
+    }
+
     return true;
   }
 }
