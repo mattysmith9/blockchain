acaf5ad01ae050851121a7894cd28dc3adb0d950
Matty Smith <matthewcsmith18@gmail.com>
2019-07-26 17:31:09 -0400

    wallet/transactions

--- wallet/index.js
+++ wallet/index.js
@@ -1,4 +1,5 @@
 const { STARTING_BALANCE } = require('../config');
+const Transaction = require('./transaction');
 const { ec, cryptoHash } = require('../utilities');
 
 class Wallet {
@@ -12,6 +13,14 @@ class Wallet {
 
   sign(data) {
     return this.keyPair.sign(cryptoHash(data));
+  }
+
+  createTransaction({ recipient, amount }) {
+    if (amount > this.balance) {
+      throw new Error('Amount exceeds balance');
+    }
+
+    return new Transaction({ senderWallet: this, recipient, amount });
   }
 }
 
