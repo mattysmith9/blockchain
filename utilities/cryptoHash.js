bb3112bc5e2b4ccf048925827ff94161ad250c36
Matty Smith <matthewcsmith18@gmail.com>
2019-07-26 21:08:40 -0400

    sha-256

--- utilities/cryptoHash.js
+++ utilities/cryptoHash.js
@@ -3,9 +3,14 @@ const crypto = require('crypto');
 const cryptoHash = (...inputs) => {
   const hash = crypto.createHash('sha256');
 
-  hash.update(inputs.sort().join(' '));
+  hash.update(
+    inputs
+      .map(input => JSON.stringify(input))
+      .sort()
+      .join(' ')
+  );
 
   return hash.digest('hex');
 };
 
-module.exports = cryptoHash; 
+module.exports = cryptoHash;
