84d0e128bfce73ed304adfafff8607dbd923b9b3
Matty Smith <matthewcsmith18@gmail.com>
2019-08-26 15:15:42 -0400

    MINING_REWARD

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,5 +1,6 @@
 const uuid = require('uuid/v1');
 const { verifySignature } = require('../utilities');
+const { REWARD_INPUT, MINING_REWARD } = require('../config');
 
 class Transaction {
   constructor({ senderWallet, recipient, amount, outputMap, input }) {
@@ -66,6 +67,13 @@ class Transaction {
     }
 
     return true;
+  }
+
+  static rewardTransaction({ minerWallet }) {
+    return new this({
+      input: REWARD_INPUT,
+      outputMap: { [minerWallet.publicKey]: MINING_REWARD }
+    });
   }
 }
 
