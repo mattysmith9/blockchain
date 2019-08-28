772a53cca276841428f1d1d0b687cce16ffdf9ea
Matty Smith <matthewcsmith18@gmail.com>
2019-08-28 15:44:22 -0400

    success

--- Blockchain/index.js
+++ Blockchain/index.js
@@ -15,7 +15,7 @@ class Blockchain {
     this.chain.push(newBlock);
   }
 
-  replaceChain(chain) {
+  replaceChain(chain, onSuccess) {
     if (chain.length <= this.chain.length) {
       console.error('the incoming chain must be longer');
       return;
@@ -25,6 +25,8 @@ class Blockchain {
       console.error('the incoming chain must be valid');
       return;
     }
+
+    if (onSuccess) onSuccess();
 
     console.log('replacing chain with', chain);
     this.chain = chain;
