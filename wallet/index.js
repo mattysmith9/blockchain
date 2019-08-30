5ae9cab1ec75d742e8ff85dc0fd7720659d4f84d
Matty Smith <matthewcsmith18@gmail.com>
2019-08-30 00:27:23 -0400

    chain.block

--- wallet/index.js
+++ wallet/index.js
@@ -13,10 +13,18 @@ class Wallet {
     return this.keyPair.sign(cryptoHash(data));
   }
 
-  createTransaction({ recipient, amount }) {
+  createTransaction({ recipient, amount, chain }) {
+    if (chain) {
+      this.balance = Wallet.calculateBalance({
+        chain,
+        address: this.publicKey
+      });
+    }
+
     if (amount > this.balance) {
       throw new Error('Amount exceeds balance');
     }
+
     return new Transaction({ senderWallet: this, recipient, amount });
   }
 
