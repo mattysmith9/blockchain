ad522c00a054d16f40cbf54339e726135add0be8
Matty Smith <matthewcsmith18@gmail.com>
2019-10-04 12:21:53 -0400

    transaction comp

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
-const Transaction = () => {
+const Transaction = ({ transaction }) => {
   const { input, outputMap } = transaction;
+  const recipients = Object.keys(outputMap);
+
+  return (
+    <div>
+      <div>
+        From: {`${input.address.substring(0, 20)}...`} | Balance: {input.amount}
+      </div>
+      {recipients.map(recipient => {
+        return (
+          <div key={recipient}>
+            To: {`${recipient.substring(0, 20)}...`} | Sent:{' '}
+            {outputMap[recipient]}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
+
+export default Transaction;
